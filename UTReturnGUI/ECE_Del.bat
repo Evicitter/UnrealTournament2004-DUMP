aa382b8c83dcc3e238688631819be98accf21597
Evicitter <evicitter@gmail.com>
2022-04-08 23:57:50 +0500

    UTReturnGUI init

---
+++ UTReturnGUI/ECE_Del.bat
@@ -0,0 +1,39 @@
+cd Animations
+if exist MechaSkaarjAnims.ukx del MechaSkaarjAnims.ukx
+if exist MetalGuardAnim.ukx del MetalGuardAnim.ukx
+if exist NecrisAnim.ukx del NecrisAnim.ukx
+if exist ONSBPAnimations.ukx del ONSBPAnimations.ukx
+cd..
+cd Maps
+if exist ONS-Adara.ut2 del ONS-Adara.ut2
+if exist ONS-IslandHop.ut2 del ONS-IslandHop.ut2
+if exist ONS-Tricky.ut2 del ONS-Tricky.ut2
+if exist ONS-Urban.ut2 del ONS-Urban.ut2
+cd..
+cd Sounds
+if exist CicadaSnds.uax del CicadaSnds.uax
+if exist DistantBooms.uax del DistantBooms.uax
+if exist ONSBPSounds.uax del ONSBPSounds.uax
+cd..
+cd StaticMeshes
+if exist BenMesh02.usx del BenMesh02.usx
+if exist BenTropicalSM01.usx del BenTropicalSM01.usx
+if exist HourAdara.usx del HourAdara.usx
+if exist ONS-BPJW1.usx del ONS-BPJW1.usx
+if exist PC_UrbanStatic.usx del PC_UrbanStatic.usx
+cd..
+cd Textures
+if exist AW-2k4XP.utx del AW-2k4XP.utx
+if exist BenTex02.utx del BenTex02.utx
+if exist BenTropical01.utx del BenTropical01.utx
+if exist BonusParticles.utx del BonusParticles.utx
+if exist CicadaTex.utx del CicadaTex.utx
+if exist Construction_S.utx del Construction_S.utx
+if exist HourAdaraTexor.utx del HourAdaraTexor.utx
+if exist ONSBP_DestroyedVehicles.utx del ONSBP_DestroyedVehicles.utx
+if exist ONSBPTextures.utx del ONSBPTextures.utx
+if exist PC_UrbanTex.utx del PC_UrbanTex.utx
+if exist UT2004ECEPlayerSkins.utx del UT2004ECEPlayerSkins.utx
+cd..
+cd System
+if exist xaplayersl3.upl del xaplayersl3.upl
